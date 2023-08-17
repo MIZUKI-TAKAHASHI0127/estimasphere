af8dd46391026a6014f57107b74f14cedc07e030
MIZUKI-TAKAHASHI0127 <blue.rose.lost.eden@gmail.com>
2023-08-17 18:09:44 +0900

    ユーザー単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+         VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+       
+         validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers' }
+         validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid' }
+         validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid' }
 end
