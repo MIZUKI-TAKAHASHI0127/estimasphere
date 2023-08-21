8bbd54b61e7a9eb44d910ee2dbc31a6459ba2a54
MIZUKI-TAKAHASHI0127 <blue.rose.lost.eden@gmail.com>
2023-08-21 19:41:42 +0900

    ルーティング変更
    ビューファイルの修正
    パスの変更

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -4,11 +4,16 @@ class RepresentativesController < ApplicationController
     @representative = @customer.representatives.build(representative_params)
     
     if @representative.save
-      redirect_to representatives_path, notice: '担当者が正常に登録されました。'
+      redirect_to customer_path(@customer), notice: '担当者が正常に登録されました。'
     else
       render :new
     end
   end
+
+  def new
+    @customer = Customer.find(params[:customer_id])
+    @representative = @customer.representatives.build
+  end  
 
   private
 
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "/bootstrap/dist/js/bootstrap.bundle.min.js"
+pin "bootstrap", to: "bootstrap/dist/js/bootstrap.bundle.js"
 pin_all_from "node_modules/stimulus/lib", under: "stimulus"
