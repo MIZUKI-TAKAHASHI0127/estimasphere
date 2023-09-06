2dcc96df10f4a7880d3a74ee00b1fa09b7edbdb9
MIZUKI-TAKAHASHI0127 <blue.rose.lost.eden@gmail.com>
2023-09-06 23:03:02 +0900

    Render用修正

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,13 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+
+
 module Estimasphere
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -16,9 +26,6 @@ module Estimasphere
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    
-    # Add the following line to enable asset compilation in production
     config.assets.compile = true
   end
 end
-
