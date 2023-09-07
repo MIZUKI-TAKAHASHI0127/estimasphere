316f531e2700af6f3662312db4029a1852045bfc
MIZUKI-TAKAHASHI0127 <blue.rose.lost.eden@gmail.com>
2023-09-07 22:44:43 +0900

    Render設定用

--- config/application.rb
+++ config/application.rb
@@ -2,11 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
-
 
 module Estimasphere
   class Application < Rails::Application
@@ -26,6 +22,9 @@ module Estimasphere
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.assets.compile = true
+    config.assets.compile = true #追加
+
+    # Add the following line to your config/application.rb
+    config.assets.enabled = true #追加
   end
 end
