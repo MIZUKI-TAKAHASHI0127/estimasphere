a60d5ae5b603f335b87baa714ff5c9affab9bf5b
MIZUKI-TAKAHASHI0127 <blue.rose.lost.eden@gmail.com>
2023-09-10 07:51:11 +0900

    Renderデプロイ

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,5 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.js"
 pin "stimulus", to: "stimulus.js"
-
-pin "controllers/sales_quotation_controller", to: "controllers/sales_quotation_controller.js"
-pin "controllers/amount_controller", to: "controllers/amount_controller.js"
-pin "controllers/hello_controller", to: "controllers/hello_controller.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
